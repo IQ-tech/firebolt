3c2f9f490afdc4a50124ff190913d6936d75073c
Italo Fontes <italo5123@hotmail.com>
2021-09-30 18:53:03 -0300

    :sparkles: created lab

--- packages/lab/src/App.js
+++ packages/lab/src/App.js
@@ -1,7 +1,13 @@
+import {useEffect} from "react"
+import a from "@iq-firebolt/client-core/lib/test.ts"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    a()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
