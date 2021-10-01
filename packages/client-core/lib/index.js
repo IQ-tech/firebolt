ef30cc62a6c1abf2a1854337d498a518d86d4b12
Italo Fontes <italo5123@hotmail.com>
2021-10-01 10:53:10 -0300

    :sparkles: imported client files

--- packages/client-core/lib/index.js
+++ packages/client-core/lib/index.js
@@ -1,4 +1,14 @@
-export { createFireboltForm } from "./create-firebolt-form"
-export { formatFormData } from "./format-data"
-export { clearFormSession } from "./session"
-export { uploadFilesToBucket } from "./requests"
+export { createFireboltForm } from "./create-firebolt-form";
+export { formatFormData } from "./format-data";
+export { clearFormSession } from "./session";
+export { default as uploadFilesToBucket } from "./requests/uploadFilesToBucket";
+
+// consts
+export { default as uiPropsPresets } from "./constants/ui-props-presets";
+
+//entities
+export { default as File } from "./entities/File";
+
+// helpers
+export { default as handleEmailAutoSuggestion } from "./helpers/handleEmailAutoSuggestion";
+export { default as getUrlParams } from "./helpers/getUrlParams";
