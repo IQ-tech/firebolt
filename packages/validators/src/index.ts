5a7807f5215d861b4d194d673f82ba26d933ba32
Italo Fontes <italo5123@hotmail.com>
2021-10-05 16:32:32 -0300

    :sparkles: exported interface

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -2,4 +2,4 @@ export { default as validators } from './validators';
 export { default as validate } from './functions/validate';
 export { default as validateGroup } from './functions/validateGroup';
 export { default as validateFBTField } from './functions/validateFBTField';
-export { default as validateFBTStep } from './functions/validateFBTStep';
+export { default as validateFBTStep, ValidateFBTStepResult } from './functions/validateFBTStep';
