4f38b6cfb2983c94ef3857c096a7eff33e71e6f2
Italo Fontes <italo5123@hotmail.com>
2021-10-25 17:24:55 -0300

    :sparkles: test

--- packages/validators/src/validators/cep/cep.test.ts
+++ packages/validators/src/validators/cep/cep.test.ts
@@ -1,7 +1,7 @@
 import isCepValid from './index';
 
 describe('CEP validation', () => {
-  test.each(['0436A000', '8999!999', '1234567', '123456789'])(
+  test.each(['0436A000', '8999!999', '1234567', '123456789', '123456789'])(
     'the value %p should not pass in  CEP validator',
     (value) => {
       expect(isCepValid.run(value).isValid).toBeFalsy();
