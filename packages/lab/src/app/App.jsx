c70f51127540628c5830ab1be59c49fdcffa25fb
Italo Fontes <italo5123@hotmail.com>
2021-10-29 17:56:01 -0300

    :sparkles: added router to debug

--- packages/lab/src/app/App.jsx
+++ packages/lab/src/app/App.jsx
@@ -1,7 +1,27 @@
 import FormDemo from "./pages/FormDemo";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <FormDemo />;
+  return (
+    <Router>
+      <div className="container">
+        <div>
+          <Link to="/debug/core">Core</Link>
+          <Link to="/debug/form">Form</Link>
+        </div>
+        <div>
+          <Switch>
+            <Route path="/debug/form">
+              <FormDemo />
+            </Route>
+            <Route exact path="/">
+              <p>home</p>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
