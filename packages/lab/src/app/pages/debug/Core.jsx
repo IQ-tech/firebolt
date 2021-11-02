2222c741bccc7f47a5a7d938859b4a54969041c8
Italo Fontes <italo5123@hotmail.com>
2021-11-02 18:49:57 -0300

    :sparkles: added missing file

--- packages/lab/src/app/pages/debug/Core.jsx
+++ packages/lab/src/app/pages/debug/Core.jsx
@@ -1,23 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import APIService from "../../../../../client-core/lib/services/API";
 
 const CoreTest = () => {
-  useEffect(() => {
-    const service = new APIService({
+  const [authKey, setAuthKey] = useState();
+  const [stepSlug, setSlug] = useState("");
+  const service = useRef(
+    new APIService({
       formAccess: {
         root: "https://btti33t5h5.execute-api.sa-east-1.amazonaws.com/dev/",
         formName: "sample",
       },
-    });
+    })
+  );
 
-    service.getStartForm('sdsf').then(data => {
-      console.log(data)
+  useEffect(() => {
+    service.current.getStartForm("sdsf").then((res) => {
+      setAuthKey(res.auth);
+      setSlug(res.step.data.slug);
     });
-
-    console.log(service);
   }, []);
 
-  return <p>debug core</p>;
+  function getNextStep() {
+    service.current
+      .getNextStep(authKey, stepSlug, {
+        stepFieldsPayload: {
+          full_name: "teste cneoura",
+          email: "batata@cenoura.com",
+        },
+      })
+      .then((res) => {
+        setAuthKey(res.auth);
+        setSlug(res.step.data.slug);
+      });
+  }
+
+  function getPreviousStep() {
+    service.current.getPreviousStep(authKey, stepSlug).then((res) => {
+      console.log(res);
+    });
+  }
+
+  return (
+    <div>
+      <p>{stepSlug} </p>
+      <button onClick={getPreviousStep}>previous</button>
+      <button onClick={getNextStep}>next</button>
+    </div>
+  );
 };
 
 export default CoreTest;
