1548055522d737cd1d1fede7f5d06c4b0069ee1b
Italo Fontes <italo5123@hotmail.com>
2021-12-22 00:23:42 -0300

    :sparkles: electron app configured

--- packages/json-schema/admin/index.js
+++ packages/json-schema/admin/index.js
@@ -1,21 +1,31 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webSecurity: false,
-    allowRunningInsecureContent: true
+    allowRunningInsecureContent: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
-  win.loadURL("http://127.0.0.1:3000/");
+  win.loadURL("http://localhost:3000/");
 
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
+
+  // waint until vite load, and reload page
+  setTimeout(() => {
+    win.reload();
+  }, [1000]);
 
   win.on("closed", function () {
     mainWindow = null;
   });
 };
+
 
 app.whenReady().then(() => {
   createWindow();
--- packages/json-schema/admin/ui/App.jsx
+++ packages/json-schema/admin/ui/App.jsx
@@ -1,13 +1,18 @@
-
+import { useEffect } from "react"
 
 const App = () => {
   function runCallback() {
     alert("is running");
   }
+  console.log(window.electron)
+
+  useEffect(() => {
+    console.log(window.electron.batata("lele"))
+  }, [])
 
   return (
     <div>
-      <p css={{color: red}}>lala</p>
+      <p css={{color: "red"}}>lala</p>
       <button onClick={runCallback}>test alert</button>
     </div>
   );
