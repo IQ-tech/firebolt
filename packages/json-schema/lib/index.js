07580b7f3ba175a7dfaf042978080b61d1d0e1c1
Italo Fontes <italo5123@hotmail.com>
2021-12-22 20:12:24 -0300

    :sparkles: validator working

--- packages/json-schema/lib/index.js
+++ packages/json-schema/lib/index.js
@@ -1,7 +1,11 @@
-'use strict';
+const jsonSchema = require("../form-schema.json");
+const Ajv = require("ajv");
 
-module.exports = jsonSchema;
-
-function jsonSchema() {
-    // TODO
+function validateJSONForm(data) {
+  const ajv = new Ajv({strict: false});
+  const validate = ajv.compile(jsonSchema);
+  const valid = validate(data);
+  return !!valid ? true : validate.errors;
 }
+
+exports.validateJSONForm = validateJSONForm;
