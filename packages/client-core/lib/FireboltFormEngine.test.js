e12ec6d7b58a7c869e2f66810d34857037136c1f
JOSE LUIZ DOS SANTOS XAVIER <joluizsantos231293@gmail.com>
2022-02-21 14:21:43 -0300

    feat: :white_check_mark: testing props:preset

--- packages/client-core/lib/FireboltFormEngine.test.js
+++ packages/client-core/lib/FireboltFormEngine.test.js
@@ -1,58 +1,59 @@
-import axios from "axios";
+import axios from "axios"
 
-import FireboltFormEngine from "./FireboltFormEngine";
+import FireboltFormEngine from "./FireboltFormEngine"
 
-import { clearAllFormSessions } from "./helpers/session/clearFormSession";
-import getFireboltLocalStorage from "./helpers/session/getFireboltLocalStorage";
-import createFormSession from "./helpers/session/createFormSession";
+import { clearAllFormSessions } from "./helpers/session/clearFormSession"
+import getFireboltLocalStorage from "./helpers/session/getFireboltLocalStorage"
+import createFormSession from "./helpers/session/createFormSession"
 
-import startFormResponse from "./__mocks__/startFormResponse";
-import nextStepFormResponse from "./__mocks__/nextStepFormResponse";
+import startFormResponse from "./__mocks__/startFormResponse"
+import nextStepWithPropsPresets from "./__mocks__/nextStepWithPropsPresets"
+import nextStepFormResponse from "./__mocks__/nextStepFormResponse"
 
-jest.mock("axios");
+jest.mock("axios")
 
-const formName = "partnerFormPotato";
+const formName = "partnerFormPotato"
 
 const form = new FireboltFormEngine({
   root: "https://my-firebolt-api/",
   formName,
-});
+})
 
 describe("start form tests", () => {
   beforeAll(() => {
-    clearAllFormSessions();
-  });
+    clearAllFormSessions()
+  })
 
   test("start form correctly creates session key on localStorage", async () => {
-    axios.get.mockResolvedValue({ data: startFormResponse });
+    axios.get.mockResolvedValue({ data: startFormResponse })
 
-    clearAllFormSessions();
+    clearAllFormSessions()
 
     // get first step
-    await form.start();
+    await form.start()
 
     // storage test
-    const fireboltLocalStorage = getFireboltLocalStorage();
-    const keys = fireboltLocalStorage?.sessionKeys || {};
-    const currentFormSession = keys?.[formName];
+    const fireboltLocalStorage = getFireboltLocalStorage()
+    const keys = fireboltLocalStorage?.sessionKeys || {}
+    const currentFormSession = keys?.[formName]
 
-    expect(currentFormSession).toBeTruthy();
+    expect(currentFormSession).toBeTruthy()
     expect(currentFormSession).toBe(
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfaWQiOiI2NzkyMTQ0MC01ZDljLTQwMmUtYTI4Yi0wZjJkOGRkNjRiZjYiLCJpYXQiOjE2MzU4NzI4NTV9.7y5GR8_1acwQ5UdttHJK0_LIoMze4COpcwy6bs4ovH4"
-    );
-  });
+    )
+  })
 
   test("start form uses session key already created on localstorage", async () => {
-    axios.get.mockResolvedValue({ data: nextStepFormResponse });
+    axios.get.mockResolvedValue({ data: nextStepFormResponse })
 
     const formAccess = {
       root: "https://another-firebolt-api/",
       formName: "myCustomForm",
-    };
-    createFormSession(formAccess.formName, "myPreviousSessionAuthKey");
+    }
+    createFormSession(formAccess.formName, "myPreviousSessionAuthKey")
 
-    const form = new FireboltFormEngine(formAccess);
-    await form.start();
+    const form = new FireboltFormEngine(formAccess)
+    await form.start()
 
     expect(axios.get).lastCalledWith(
       "https://another-firebolt-api/form/myCustomForm",
@@ -61,38 +62,48 @@ describe("start form tests", () => {
           authorization: "Bearer myPreviousSessionAuthKey",
         }),
       })
-    );
-  });
+    )
+  })
 
-  test.todo("throw an error when providing invalid api access");
+  test.todo("throw an error when providing invalid api access")
 
-  test.todo("throw an error on try to debug a step without debug key");
-});
+  test.todo("throw an error on try to debug a step without debug key")
+})
 
-describe('tests about the form autofill by base64 at URL', () => {
-  const autoFillBase64 = 'autofill=JTdCJTI3bmFtZSUyNyUzQSU3QiUyN3ZhbHVlJTI3JTNBJTI3UnVhbiUyMEJlcnQlQzMlQTklMjclMkMlMjdtYXNrJTI3JTNBJTI3JTI3JTdEJTJDJTI3Y3BmJTI3JTNBJTdCJTI3dmFsdWUlMjclM0ElMjc0NTAuNTkyLjczOC01NyUyNyUyQyUyN21hc2slMjclM0ElMjdjcGYlMjclN0QlMkMlMjdlbWFpbCUyNyUzQSU3QiUyN3ZhbHVlJTI3JTNBJTI3YmVydGUucnVhbiU0MGdtYWlsLmNvbSUyNyUyQyUyN21hc2slMjclM0ElMjclMjclN0QlMkMlMjdpbmNvbWUlMjclM0ElN0IlMjd2YWx1ZSUyNyUzQSUyNzYwMDAlMjclMkMlMjdtYXNrJTI3JTNBJTI3bW9uZXklMjclN0QlMkMlMjdwaG9uZSUyNyUzQSU3QiUyN3ZhbHVlJTI3JTNBJTI3NDI5OTk4ODM3NjglMjclMkMlMjdtYXNrJTI3JTNBJTI3cGhvbmVfbnVtYmVyJTI3JTdEJTdE';
+describe("tests about the form autofill by base64 at URL", () => {
+  const autoFillBase64 =
+    "autofill=JTdCJTI3bmFtZSUyNyUzQSU3QiUyN3ZhbHVlJTI3JTNBJTI3UnVhbiUyMEJlcnQlQzMlQTklMjclMkMlMjdtYXNrJTI3JTNBJTI3JTI3JTdEJTJDJTI3Y3BmJTI3JTNBJTdCJTI3dmFsdWUlMjclM0ElMjc0NTAuNTkyLjczOC01NyUyNyUyQyUyN21hc2slMjclM0ElMjdjcGYlMjclN0QlMkMlMjdlbWFpbCUyNyUzQSU3QiUyN3ZhbHVlJTI3JTNBJTI3YmVydGUucnVhbiU0MGdtYWlsLmNvbSUyNyUyQyUyN21hc2slMjclM0ElMjclMjclN0QlMkMlMjdpbmNvbWUlMjclM0ElN0IlMjd2YWx1ZSUyNyUzQSUyNzYwMDAlMjclMkMlMjdtYXNrJTI3JTNBJTI3bW9uZXklMjclN0QlMkMlMjdwaG9uZSUyNyUzQSU3QiUyN3ZhbHVlJTI3JTNBJTI3NDI5OTk4ODM3NjglMjclMkMlMjdtYXNrJTI3JTNBJTI3cGhvbmVfbnVtYmVyJTI3JTdEJTdE"
 
   beforeEach(() => {
-    clearAllFormSessions();
-  
-    axios.get.mockResolvedValue({ data: startFormResponse });
+    clearAllFormSessions()
 
-    Object.defineProperty(window, 'location', {
+    axios.get.mockResolvedValue({ data: startFormResponse })
+
+    Object.defineProperty(window, "location", {
       writable: true,
       value: {
         search: autoFillBase64,
-        href: autoFillBase64
-      }
-    });
-    
-  });
+        href: autoFillBase64,
+      },
+    })
+  })
 
   test("form.start() must autofill value prop of the field email at the fields array", async () => {
     // get first step
-    const formStartResult = await form.start();
+    const formStartResult = await form.start()
 
-    expect(formStartResult.step.data.fields[1]).toHaveProperty('value');
-    expect(formStartResult.step.data.fields[1].value).toBe('berte.ruan@gmail.com')
-  });
+    expect(formStartResult.step.data.fields[1]).toHaveProperty("value")
+    expect(formStartResult.step.data.fields[1].value).toBe(
+      "berte.ruan@gmail.com"
+    )
+  })
+})
 
+describe("testing props:preset", () => {
+  test("form.start() should apply props:preset", async () => {
+    axios.get.mockResolvedValue({ data: nextStepWithPropsPresets })
+    const formStartResult = await form.start()
+
+    expect(formStartResult?.step?.data?.fields[0]?.["ui:props"].label).toBe("CEP")
+  })
 })
--- packages/client-core/lib/__mocks__/nextStepWithPropsPresets.js
+++ packages/client-core/lib/__mocks__/nextStepWithPropsPresets.js
@@ -1,161 +1,160 @@
 const nextStepWithPropsPresets = {
-  "step": {
-    "data": {
-      "slug": "address",
-      "type": "form",
-      "friendlyname": "Endereço",
-      "sharedata": true,
-      "fields": [
+  message: "ok",
+  formData: {
+    auth: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfaWQiOiI2NzkyMTQ0MC01ZDljLTQwMmUtYTI4Yi0wZjJkOGRkNjRiZjYiLCJpYXQiOjE2MzU4NzI4NTV9.7y5GR8_1acwQ5UdttHJK0_LIoMze4COpcwy6bs4ovH4",
+    meta: {
+      lastStep: 5,
+      forms: [
         {
-          "slug": "zipcode",
-          "ui:widget": "CEP",
-          "ui:props-preset": "br-cep",
-          "ui:props": {
-            "relatedFieldsSlugs": {
-              "cityFieldSlug": "city",
-              "stateFieldSlug": "state",
-              "streetFieldSlug": "street_address",
-              "additionalAddressFieldSlug": "additional_info",
-              "neighborhoodFieldSlug": "neighborhood",
-            },
-          },
-          "validators": [
-            {
-              "type": "required",
-            },
-            {
-              "type": "cep",
-            },
-          ],
-          "value": "13224-410",
+          position: 1,
+          slug: "personal_data",
+          friendlyName: "Vamos começar",
         },
         {
-          "slug": "state",
-          "ui:widget": "Select",
-          "ui:props-preset": "br-states",
-          "ui:props": {
-            "label": "Estado",
-          },
-          "validators": [
-            {
-              "type": "required",
-            },
-          ],
-          "meta": {},
-          "value": "BR-SP",
+          position: 2,
+          slug: "documents",
+          friendlyName: "Documentos",
         },
         {
-          "slug": "city",
-          "ui:widget": "BRCity",
-          "ui:props": {
-            "label": "Cidade",
-            "stateFieldSlug": "state",
-          },
-          "validators": [
-            {
-              "type": "required",
-            },
-            {
-              "type": "nonNumeric",
-            },
-          ],
-          "meta": {},
-          "value": "Várzea Paulista",
+          position: 3,
+          slug: "address",
+          friendlyName: "Endereço",
         },
         {
-          "slug": "street_address",
-          "ui:widget": "Text",
-          "ui:props": {
-            "label": "Endereço",
-            "placeholder": "Ex.: Av Paulista",
-          },
-          "validators": [
-            {
-              "type": "required",
-            },
-          ],
-          "value": "Rua Artibano Murari",
+          position: 4,
+          slug: "bills",
+          friendlyName: "Adicionar Contas",
         },
         {
-          "slug": "street_number",
-          "ui:widget": "Number",
-          "ui:props": {
-            "label": "Número",
-          },
-          "validators": [
-            {
-              "type": "required",
-            },
-          ],
-          "meta": {},
-          "value": "123",
-        },
-        {
-          "slug": "additional_info",
-          "ui:widget": "Text",
-          "ui:props": {
-            "label": "Complemento",
-            "placeholder": "Ex.: Apto 25 bl C",
-          },
-          "meta": {},
-        },
-        {
-          "slug": "neighborhood",
-          "ui:widget": "Text",
-          "ui:props": {
-            "label": "Bairro",
-            "placeholder": "Ex.: Centro",
-            "readonly": true,
-          },
-          "validators": [
-            {
-              "type": "required",
-            },
-          ],
-          "meta": {},
-          "value": "Jardim Cruz Alta",
+          position: 5,
+          slug: "token",
+          friendlyName: "Token",
         },
       ],
     },
-    "position": 3,
+    capturedData: {},
+    step: {
+      data: {
+        id: 1,
+        slug: "personal_data",
+        type: "form",
+        friendlyName: "Vamos começar",
+        fields: [
+          {
+            "slug": "zipcode",
+            "ui:widget": "CEP",
+            "ui:props-preset": "br-cep",
+            "ui:props": {
+              "relatedFieldsSlugs": {
+                "cityFieldSlug": "city",
+                "stateFieldSlug": "state",
+                "streetFieldSlug": "street_address",
+                "additionalAddressFieldSlug": "additional_info",
+                "neighborhoodFieldSlug": "neighborhood",
+              },
+            },
+            "validators": [
+              {
+                "type": "required",
+              },
+              {
+                "type": "cep",
+              },
+            ],
+            "value": "13224-410",
+          },
+          {
+            "slug": "state",
+            "ui:widget": "Select",
+            "ui:props-preset": "br-states",
+            "ui:props": {
+              "label": "Estado",
+            },
+            "validators": [
+              {
+                "type": "required",
+              },
+            ],
+            "meta": {},
+            "value": "BR-SP",
+          },
+          {
+            "slug": "city",
+            "ui:widget": "BRCity",
+            "ui:props": {
+              "label": "Cidade",
+              "stateFieldSlug": "state",
+            },
+            "validators": [
+              {
+                "type": "required",
+              },
+              {
+                "type": "nonNumeric",
+              },
+            ],
+            "meta": {},
+            "value": "Várzea Paulista",
+          },
+          {
+            "slug": "street_address",
+            "ui:widget": "Text",
+            "ui:props": {
+              "label": "Endereço",
+              "placeholder": "Ex.: Av Paulista",
+            },
+            "validators": [
+              {
+                "type": "required",
+              },
+            ],
+            "value": "Rua Artibano Murari",
+          },
+          {
+            "slug": "street_number",
+            "ui:widget": "Number",
+            "ui:props": {
+              "label": "Número",
+            },
+            "validators": [
+              {
+                "type": "required",
+              },
+            ],
+            "meta": {},
+            "value": "123",
+          },
+          {
+            "slug": "additional_info",
+            "ui:widget": "Text",
+            "ui:props": {
+              "label": "Complemento",
+              "placeholder": "Ex.: Apto 25 bl C",
+            },
+            "meta": {},
+          },
+          {
+            "slug": "neighborhood",
+            "ui:widget": "Text",
+            "ui:props": {
+              "label": "Bairro",
+              "placeholder": "Ex.: Centro",
+              "readonly": true,
+            },
+            "validators": [
+              {
+                "type": "required",
+              },
+            ],
+            "meta": {},
+            "value": "Jardim Cruz Alta",
+          },
+        ],
+      },
+      position: 1,
+    },
   },
-  "meta": {
-    "lastStep": "invoice_expiration",
-    "forms": [
-      {
-        "position": 1,
-        "slug": "personal_data_1",
-        "friendlyname": "Dados pessoais",
-      },
-      {
-        "position": 2,
-        "slug": "personal_data_2",
-        "friendlyname": "Dados complementares",
-      },
-      {
-        "position": 3,
-        "slug": "address",
-        "friendlyname": "Endereço",
-      },
-      {
-        "position": 4,
-        "slug": "occupation",
-        "friendlyname": "Dados Profissionais",
-      },
-      {
-        "position": 5,
-        "slug": "personal_documents",
-        "friendlyname": "Documentos",
-      },
-      {
-        "position": 6,
-        "slug": "invoice_expiration",
-        "friendlyname": "Personalização do cartão",
-      },
-    ],
-  },
-  "auth":
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfaWQiOiI0ZmQxYjZkNi1hOGQzLTQ3NmQtYmFhNy0yOWNkNjg4MzAzOTkiLCJpYXQiOjE2NDQ0MzU1MTV9._3Vqg3fMu_jECU-zSasAX_feUPn93QcZeDPN1BWM9bA",
+  errorData: {},
 }
 
 export default nextStepWithPropsPresets
