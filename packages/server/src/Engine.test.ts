13e1c2bb4291c316c604ab0414ec54a6fd2d810f
Italo Fontes <italo5123@hotmail.com>
2022-03-18 18:14:56 -0300

    test(server): :white_check_mark: defining design through tests

--- packages/server/src/Engine.test.ts
+++ packages/server/src/Engine.test.ts
@@ -1,3 +1,35 @@
 describe("start stepper engine", () => {
-  test.todo("Engine.start should ")
+  test.todo(
+    "Engine.start should generate a new token and return the first step"
+  )
+
+  test.todo(
+    "Engine.start should use an existing token and retrieve the last uncompleted step"
+  )
+})
+
+describe("next step handling", () => {
+  test.todo("Engine.proceed should succesfully validate the step fields")
+
+  test.todo(
+    "Engine.proceed should validate the step fields and return an error"
+  )
+})
+
+describe("Props presets apply", () => {
+  test.todo(
+    "props presets should be applied on Engine.start return value"
+  )
+
+  test.todo(
+    "props presets should be applied on Engine.proceed return value"
+  )
+
+  test.todo(
+    "props presets should be applied on Engine.goBack return value"
+  )
+
+  test.todo(
+    "props presets should be applied on Engine.debug return value"
+  )
 })
