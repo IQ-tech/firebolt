1dbeecd5eefe08937ee4f84f78cda493d1fcfe40
Vaz-Tiago <tvaz@redventures.com>
2022-03-22 14:10:47 -0300

    test(server): 🔧 add localStorage mock config

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,5 +1,6 @@
-const jestBaseConfig = require("../../jest.base.config");
+const jestBaseConfig = require("../../jest.base.config")
 
 module.exports = {
   ...jestBaseConfig,
+  setupFiles: ["<rootDir>/__jest__/browserMocks.js"],
 }
