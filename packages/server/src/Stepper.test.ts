1bd33c513bd3610c692240198327dd0b1e661724
Vaz-Tiago <tvaz@redventures.com>
2022-03-25 15:20:30 -0300

    test(server): ✅ add proceed first step

--- packages/server/src/Stepper.test.ts
+++ packages/server/src/Stepper.test.ts
@@ -1,12 +1,58 @@
 import faker from "faker"
-import JSONSample from "./json-sample.json"
 import Stepper from "./Stepper"
+import { IExperienceJSONSchema } from "./types"
+import { IEngineResolvers, IFireboltSession } from "./interfaces/IEngine"
 
+import JSONSample from "./json-new-sample.json"
 const localStorage = global.localStorage
+const mockedGetFormJSONSchema = jest.fn(
+  async () => ({} as IExperienceJSONSchema)
+)
+
+const mockedGetSession = jest.fn(async (sessionId?: string) => {
+  const session = localStorage.getItem(sessionId)
+  if (session) return JSON.parse(session) as IFireboltSession
+  return undefined
+})
+
+const mockedSetSession = jest.fn(async (stepData: IFireboltSession) => {
+  localStorage.setItem(stepData.sessionId, JSON.stringify(stepData))
+})
 
 describe("Stepper.Proceed handling", () => {
-  test.todo("should start a new experience")
-  test.todo("should identify an started experience and return the correct step")
+  beforeEach(() => {
+    localStorage.clear()
+    jest.clearAllMocks()
+  })
+
+  test.todo("should return a error when schema is not found")
+  test.todo("should return a error when schema dont have default flow")
+
+  test("should start a new experience", async () => {
+    const sample = JSONSample as IExperienceJSONSchema
+    const resolvers: IEngineResolvers = {
+      getFormJSONSchema: mockedGetFormJSONSchema,
+      getSession: mockedGetSession,
+      setSession: mockedSetSession,
+    }
+
+    const fireboltStepper = new Stepper({
+      experienceId: "sample",
+      experienceJSONSchema: sample,
+      resolvers,
+    })
+
+    const firstStep = await fireboltStepper.proceed()
+
+    expect(firstStep).toHaveProperty("sessionId")
+    expect(firstStep.step.slug).toBe("personal_data")
+    expect(firstStep.capturedData).toEqual({})
+  })
+
+  test.todo(
+    "should identify an started experience and return the correct step",
+    async () => {}
+  )
   test.todo("should validate the step fields and return an error")
   test.todo("should validate the step fields and return the next step info")
   test.todo("should be able update previous steps without data loss")
