6a781f4103ada2bfed1cf1f12f3caf0514d75cf5
Italo Fontes <italo5123@hotmail.com>
2022-07-11 14:01:01 -0300

    feat(core-validators): added morethan validator rule

--- packages/validate/src/rules/lessThan/lessThan.test.ts
+++ packages/validate/src/rules/lessThan/lessThan.test.ts
@@ -4,7 +4,7 @@ describe("Rule - lessThan", () => {
   test.each([
     { value: 1, maxValue: 5 },
     { value: '20', maxValue: 50 },
-  ])("Success Case (lessThan): '$value' < '$maxValue'", ({ value, maxValue }) => {
+  ])("Success Case: '$value' < $maxValue", ({ value, maxValue }) => {
     const {isValid, givenValue} = lessThan(value, { properties: { maxValue }})
     expect(isValid).toBeTruthy()
     expect(givenValue).toBe(value)
@@ -13,10 +13,10 @@ describe("Rule - lessThan", () => {
   test.each([
     { value: 15, maxValue: 6 },
     { value: '20', maxValue: 15 },
-  ])("Fail Case (lessThan): '$value' > '$maxValue'", ({ value, maxValue }) => {
+  ])("Fail Case: '$value' > $maxValue", ({ value, maxValue }) => {
     const {isValid, message, givenValue} = lessThan(value, { properties: { maxValue }})
     expect(isValid).toBeFalsy()
-    expect(message).toBe(`Value '${value}' is less than the minimum value required: ${maxValue}`)
+    expect(message).toBe(`Value '${value}' is bigger than the maximun value required: ${maxValue}`)
     expect(givenValue).toBe(value)
   })
 })
--- packages/validate/src/rules/moreThan/moreThan.test.ts
+++ packages/validate/src/rules/moreThan/moreThan.test.ts
@@ -4,7 +4,7 @@ describe("Rule - MoreThan", () => {
   test.each([
     { value: 5, minValue: 1 },
     { value: '20', minValue: 2 },
-  ])("Success Case (MoreThan): '$value' > '$minValue'", ({ value, minValue }) => {
+  ])("Success Case: '$value' > '$minValue'", ({ value, minValue }) => {
     const {isValid, givenValue} = moreThan(value, { properties: { minValue }})
     expect(isValid).toBeTruthy()
     expect(givenValue).toBe(value)
@@ -13,7 +13,7 @@ describe("Rule - MoreThan", () => {
   test.each([
     { value: 5, minValue: 6 },
     { value: '20', minValue: 50 },
-  ])("Fail Case (moreThan): '$value' < '$minValue'", ({ value, minValue }) => {
+  ])("Fail Case: '$value' < '$minValue'", ({ value, minValue }) => {
     const {isValid, message, givenValue} = moreThan(value, { properties: { minValue }})
     expect(isValid).toBeFalsy()
     expect(message).toBe(`Value '${value}' is less than the minimum value required: ${minValue}`)
