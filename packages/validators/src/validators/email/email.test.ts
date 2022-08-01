701adba3b38754c1bd7eb5b91b11db17b5f578f2
Vaz-Tiago <tvaz@redventures.com>
2022-08-01 12:16:21 -0300

    fix(validators): 🩹 add accents validator on email

--- packages/validators/src/validators/email/email.test.ts
+++ packages/validators/src/validators/email/email.test.ts
@@ -1,50 +1,58 @@
-import faker from 'faker';
-import isValidEmail from './index';
+import faker from "faker"
+import isValidEmail from "./index"
 
 const checkingValidEmail = ({
   min,
   max,
   locale,
 }: {
-  min: number;
-  max: number;
-  locale: string;
+  min: number
+  max: number
+  locale: string
 }) => {
-  faker.locale = locale;
-  let i = min;
+  faker.locale = locale
+  let i = min
 
   for (i; i < max; i++) {
-    const email = faker.internet.email();
-    expect(isValidEmail.run(email).isValid).toBeTruthy();
+    const email = faker.internet.email()
+    expect(isValidEmail.run(email).isValid).toBeTruthy()
   }
-};
+}
 
-describe('email validation', () => {
-  test('should all emails that faker generates with default `pt-br` will be validated', () => {
-    checkingValidEmail({ min: 0, max: 10000, locale: 'pt_BR' });
-  });
+describe("email validation", () => {
+  test("should all emails that faker generates with default `pt-br` will be validated", () => {
+    checkingValidEmail({ min: 0, max: 10000, locale: "pt_BR" })
+  })
 
-  test('should all emails that faker generates with default `en` will be validated', () => {
-    checkingValidEmail({ min: 0, max: 10000, locale: 'en' });
-  });
+  test("should all emails that faker generates with default `en` will be validated", () => {
+    checkingValidEmail({ min: 0, max: 10000, locale: "en" })
+  })
+})
 
-  test.each([
-    '#@%^%#$@#$@#.com',
-    '@example.com',
-    'Joe Smith <email@example.com>',
-    'email.example.com',
-    'email@example@example.com',
-    '.email@example.com',
-    'email.@example.com',
-    'email..email@example.com',
-    'email@example.com (Joe Smith)',
-    'email@example',
-    'email@-example.com',
-    'email@111.222.333.44444',
-    'email@example..com',
-    'Abc..123@example.com',
-    'P*****4@GMAIL.COM'
-  ])('should the function to indicate that the email is invalid', (value) => {
-    expect(isValidEmail.run(value).isValid).toBeFalsy();
-  });
-});
+describe.each([
+  "#@%^%#$@#$@#.com",
+  "@example.com",
+  "Joe Smith <email@example.com>",
+  "email.example.com",
+  "email@example@example.com",
+  ".email@example.com",
+  "email.@example.com",
+  "email..email@example.com",
+  "email@example.com (Joe Smith)",
+  "email@example",
+  "email@-example.com",
+  "email@111.222.333.44444",
+  "email@example..com",
+  "Abc..123@example.com",
+  "P*****4@GMAIL.COM",
+  "émail@example.com",
+  "emaiû@example.com",
+  "emãil@example.com",
+  "emaìl@example.com",
+  "emõil@example.com",
+  "emçil@example.com",
+])("characters email validation", (value) => {
+  test(`Email (${value}) must be invalid`, () => {
+    expect(isValidEmail.run(value).isValid).toBeFalsy()
+  })
+})
