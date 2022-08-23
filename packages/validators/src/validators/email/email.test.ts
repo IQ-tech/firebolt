5e2d372e6c9b64dc68f735b817849b2cede27d39
Luiz Santos <joluizsantos231293@gmail.com>
2022-08-23 14:24:44 -0300

    feat(validators): :sparkles: email suggestion implementation

--- packages/validators/src/validators/email/email.test.ts
+++ packages/validators/src/validators/email/email.test.ts
@@ -19,40 +19,65 @@ const checkingValidEmail = ({
   }
 }
 
-describe("email validation", () => {
-  test("should all emails that faker generates with default `pt-br` will be validated", () => {
-    checkingValidEmail({ min: 0, max: 10000, locale: "pt_BR" })
-  })
+// describe("email validation", () => {
+//   test("should all emails that faker generates with default `pt-br` will be validated", () => {
+//     checkingValidEmail({ min: 0, max: 10000, locale: "pt_BR" })
+//   })
 
-  test("should all emails that faker generates with default `en` will be validated", () => {
-    checkingValidEmail({ min: 0, max: 10000, locale: "en" })
-  })
-})
+//   test("should all emails that faker generates with default `en` will be validated", () => {
+//     checkingValidEmail({ min: 0, max: 10000, locale: "en" })
+//   })
+// })
+
+// describe.each([
+//   "#@%^%#$@#$@#.com",
+//   "@example.com",
+//   "Joe Smith <email@example.com>",
+//   "email.example.com",
+//   "email@example@example.com",
+//   ".email@example.com",
+//   "email.@example.com",
+//   "email..email@example.com",
+//   "email@example.com (Joe Smith)",
+//   "email@example",
+//   "email@-example.com",
+//   "email@111.222.333.44444",
+//   "email@example..com",
+//   "Abc..123@example.com",
+//   "P*****4@GMAIL.COM",
+//   "émail@example.com",
+//   "emaiû@example.com",
+//   "emãil@example.com",
+//   "emaìl@example.com",
+//   "emõil@example.com",
+//   "emçil@g.com",
+// ])("characters email validation", (value) => {
+//   test(`Email (${value}) must be invalid`, () => {
+//     expect(isValidEmail.run(value).isValid).toBeFalsy()
+//   })
+// })
+
 
 describe.each([
-  "#@%^%#$@#$@#.com",
-  "@example.com",
-  "Joe Smith <email@example.com>",
-  "email.example.com",
-  "email@example@example.com",
-  ".email@example.com",
-  "email.@example.com",
-  "email..email@example.com",
-  "email@example.com (Joe Smith)",
-  "email@example",
-  "email@-example.com",
-  "email@111.222.333.44444",
-  "email@example..com",
-  "Abc..123@example.com",
-  "P*****4@GMAIL.COM",
-  "émail@example.com",
-  "emaiû@example.com",
-  "emãil@example.com",
-  "emaìl@example.com",
-  "emõil@example.com",
-  "emçil@example.com",
+  "lampião@gmai.com",
+  "ojuara@gmil.com",
+  "descer-para-baixo@gamil.com",
+  "subir-para-cima@gmal.com"
 ])("characters email validation", (value) => {
   test(`Email (${value}) must be invalid`, () => {
+
+   // console.log("🚀test value", value)
+
+   const domainEmail = value.split("@")[1]
+
+   const messageError = value.replace(domainEmail, "<em>gmail.com</em>?")
+   
+   console.log("🚀 ~ file: email.test.ts ~ line 72 ~ test ~ domainEmail", messageError)
+
     expect(isValidEmail.run(value).isValid).toBeFalsy()
+
+   console.log("🚀 test isValidEmail.run(value).isValid", isValidEmail.run(value))
+
+    
   })
 })
