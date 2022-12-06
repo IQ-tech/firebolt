a7a3800cf747ef36fbe9b2f41347604ec8f7b1f5
Andre Cabral <cabral.dev@gmail.com>
2022-12-06 17:17:08 -0300

    feat: character ' - ' is forbidden on the validator hasSpecialCharacters

--- packages/validators/src/validators/noSpecialCharacters/index.ts
+++ packages/validators/src/validators/noSpecialCharacters/index.ts
@@ -2,7 +2,7 @@ import { ValidationResult, Validator } from '../../classes';
 
 function hasSpecialCharacters(text = ''): ValidationResult {
   if (!text) return new ValidationResult(true);
-  const isValid = !text.trim().match(/[~!@#$%^&*()_+=`{}[\]|\\:;'<>,./?]/gi);
+  const isValid = !text.trim().match(/[~!@#$%^&*()-_+=`{}[\]|\\:;'<>,./?]/gi);
   const message = isValid
     ? ''
     : 'Esse campo não deve conter caracteres especiais';
