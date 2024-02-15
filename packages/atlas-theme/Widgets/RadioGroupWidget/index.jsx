0920eca8beeaadad9db7ecd386265d706672ba41
qnedy <altmaisefequatro@gmail.com>
2024-02-15 17:06:36 -0300

    fix: 🔥 remove material dependencies and return workflow node to 14

--- packages/atlas-theme/Widgets/RadioGroupWidget/index.jsx
+++ packages/atlas-theme/Widgets/RadioGroupWidget/index.jsx
@@ -1,10 +1,4 @@
 import React from "react"
-import FormControl from "@material-ui/core/FormControl"
-import FormLabel from "@material-ui/core/FormLabel"
-import RadioGroup from "@material-ui/core/RadioGroup"
-import FormControlLabel from "@material-ui/core/FormControlLabel"
-import Radio from "@material-ui/core/Radio"
-import FormHelperText from "@material-ui/core/FormHelperText"
 import FieldHolder from "../../FieldHolder"
 
 import "./styles.scss"
