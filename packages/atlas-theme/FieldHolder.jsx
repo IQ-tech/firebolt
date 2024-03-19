dcf1d78e2c86658631703781da976984da51d830
qnedy <altmaisefequatro@gmail.com>
2024-03-19 17:04:24 -0300

    fix: 💄 radio group

--- packages/atlas-theme/FieldHolder.jsx
+++ packages/atlas-theme/FieldHolder.jsx
@@ -9,15 +9,6 @@ const FieldHolder = ({ useLabel = false, fieldName, label, children }) => {
 
       {children}
     </div>
-
-  //<div
-  //   class="min-max-info-container"
-  //   *ngIf="this.showAmountMinMaxMessage">
-  //   <p>
-  //     Valor mínimo de {{ this.minLoanAmount | currency }} e máximo de
-  //     {{ this.maxLoanAmount | currency }}
-  //   </p>
-  // </div> usar o JSON pra add dps do field)
   )
 }
 
