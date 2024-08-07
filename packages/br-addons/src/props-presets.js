fdcfa9eb9e6805794fd5343a2c2958549b2e3b97
Teresa Lazarini <teresalazarini@gmail.com>
2024-08-07 16:35:45 -0300

    fix: update placeholder again

--- packages/br-addons/src/props-presets.js
+++ packages/br-addons/src/props-presets.js
@@ -41,7 +41,7 @@ const UIPropsPresetsList = {
     options: NATIONALITIES,
   },
   "br-rg": {
-    placeholder: "00.000.000-00",
+    placeholder: "00.000.000-0",
     label: "RG",
     mask: RGMask,
   },
