3b571d1b5723e52e7f4fe3861b9df56ed9c686d8
gi-flor <gisele.araujo.dev@gmail.com>
2025-07-28 15:32:47 -0300

    feat: open tooltip when label is clicked

--- packages/aurora-theme/wrappers/LabelHolder/index.tsx
+++ packages/aurora-theme/wrappers/LabelHolder/index.tsx
@@ -5,15 +5,22 @@ import {
 } from "@consumidor-positivo/aurora"
 
 import "./styles.scss"
+import { useState } from "react"
 
 export const LabelHolder = ({ label, tooltipText }) => {
+  const [openTooltip, setOpenTooltip] = useState(false)
+
+  function toggleTooltip() {
+    setOpenTooltip(!openTooltip)
+  }
+
   return (
     <Conditional
       condition={tooltipText}
       renderIf={
-        <div className="label-holder__with-tooltip">
+        <div className="label-holder__with-tooltip" onClick={toggleTooltip}>
           {label}{" "}
-          <Tooltip text={tooltipText} position="top">
+          <Tooltip text={tooltipText} position="top" open={openTooltip}>
             <IconHelpCircle />
           </Tooltip>
         </div>
