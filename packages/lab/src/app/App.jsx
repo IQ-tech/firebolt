3118790c23cadb582eeef83d42891f3228997f45
gi-flor <gisele.araujo.dev@gmail.com>
2025-12-22 18:18:43 -0300

    refactor: 404 page lap

--- packages/lab/src/app/App.jsx
+++ packages/lab/src/app/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Header from "./components/Header"
 import "iq-blueberry/dist/styles.css"
 import "./darkmode.css"
+import { NotFound } from "./components/NotFound"
 
 const App = () => {
   return (
@@ -22,19 +23,7 @@ const App = () => {
               <p>home</p>
             </Route>
             <Route exact path="*">
-              <h1
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAuhF8WgdyUkYM7UtRzt0ZbiwWbN_SlShhUuOX766kRAKtgXKKwmuZUO5eFCBIXC5VcXM&usqp=CAU"
-                  alt="Algo de errado não está certo."
-                />
-                <p style={{ fontSize: "100px" }}>&#129300;404&#128579;</p>
-              </h1>
+              <NotFound />
             </Route>
           </Switch>
         </div>
---
+++ packages/lab/src/app/components/NotFound/index.jsx
@@ -0,0 +1,19 @@
+export const NotFound = () => {
+  return (
+    <h1
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        height: "80vh",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAuhF8WgdyUkYM7UtRzt0ZbiwWbN_SlShhUuOX766kRAKtgXKKwmuZUO5eFCBIXC5VcXM&usqp=CAU"
+        alt="Algo de errado não está certo."
+      />
+      <p style={{ fontSize: "100px" }}>&#129300;404&#128579;</p>
+    </h1>
+  )
+}
